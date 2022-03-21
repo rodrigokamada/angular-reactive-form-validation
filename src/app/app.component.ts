8b3bcfedf80f85f631c8c0ceb854a9d97d4459a9
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-03-20 21:44:32 -0300

    Adding the reactive form validation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { emailValidator } from './email-validator.directive';
+
+interface IUser {
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  showPassword: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-reactive-form-validation';
+export class AppComponent implements OnInit {
+
+  reactiveForm!: FormGroup;
+  user: IUser;
+
+  constructor() {
+    this.user = {} as IUser;
+  }
+
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      name: new FormControl(this.user.name, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(250),
+      ]),
+      nickname: new FormControl(this.user.nickname, [
+        Validators.maxLength(10),
+      ]),
+      email: new FormControl(this.user.email, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(250),
+        emailValidator(),
+      ]),
+      password: new FormControl(this.user.password, [
+        Validators.required,
+        Validators.minLength(15),
+      ]),
+    });
+  }
+
+  get name() {
+    return this.reactiveForm.get('name')!;
+  }
+
+  get nickname() {
+    return this.reactiveForm.get('nickname')!;
+  }
+
+  get email() {
+    return this.reactiveForm.get('email')!;
+  }
+
+  get password() {
+    return this.reactiveForm.get('password')!;
+  }
+
+  public validate(): void {
+    if (this.reactiveForm.invalid) {
+      for (const control of Object.keys(this.reactiveForm.controls)) {
+        this.reactiveForm.controls[control].markAsTouched();
+      }
+      return;
+    }
+
+    this.user = this.reactiveForm.value;
+
+    console.info('Name:', this.user.name);
+    console.info('Nickname:', this.user.nickname);
+    console.info('Email:', this.user.email);
+    console.info('Password:', this.user.password);
+  }
+
 }
