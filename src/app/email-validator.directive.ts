51101cfc4b0b5c3ca517859c50d680cd8ba63493
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-03-20 21:42:35 -0300

    Improvements.

--- src/app/email-validator.directive.ts
+++ src/app/email-validator.directive.ts
@@ -1,5 +1,25 @@
 import { Directive } from '@angular/core';
-import { NG_VALIDATORS, AbstractControl, Validator, ValidationErrors } from '@angular/forms';
+import { NG_VALIDATORS, AbstractControl, Validator, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function emailValidator(): ValidatorFn {
+
+  const EMAIL_REGEXP = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  return (control: AbstractControl): ValidationErrors | null => {
+    const isValid = EMAIL_REGEXP.test(control.value);
+
+    if (isValid) {
+      return null;
+    } else {
+      return {
+        emailValidator: {
+          valid: false,
+        },
+      };
+    }
+  };
+
+}
 
 @Directive({
   selector: '[appEmailValidator]',
@@ -11,23 +31,11 @@ import { NG_VALIDATORS, AbstractControl, Validator, ValidationErrors } from '@an
 })
 export class EmailValidatorDirective implements Validator {
 
-  private EMAIL_REGEXP = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
   constructor() {
   }
 
   public validate(control: AbstractControl): ValidationErrors | null {
-    const isValid = this.EMAIL_REGEXP.test(control.value);
-
-    if (isValid) {
-      return null;
-    } else {
-      return {
-        emailValidator: {
-          valid: false,
-        },
-      };
-    }
+    return emailValidator()(control);
   }
 
 }
